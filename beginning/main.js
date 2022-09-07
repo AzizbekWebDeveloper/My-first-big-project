7b9f9d3c30e4e3eaccb2ae01e4615f7b7a965682
Azizbek_Minecraftter <saloom1030@gmail.com>
2022-09-07 14:59:14 +0300

    Today I add any details and fix any bugs!

--- beginning/main.js
+++ beginning/main.js
@@ -1,5 +1,5 @@
 console.log("Hello everyone");
 console.log("It is my first big project!");
 // console.log("dnfbvj");
-// console.log("dvmv n");
-// console.log("mkyhfjj");
+// consg
+console.log("Good bye!")
